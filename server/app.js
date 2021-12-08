6e6bdb41b2fa665e631f68a6414d2ae949df4a20
andrei.odobesteanu <alexandru.odobesteanu@yahoo.com>
2021-12-08 21:20:54 +0200

    IMPLEMENTARE ADAUGARE BD

--- server/app.js
+++ server/app.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 const mongoose = require('mongoose')
+const { MONGOURI } = require('./keys')
+
+require('./models/user')
+
+app.use(express.json())
+app.use(require("./routes/auth"))
+
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongo")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log("err connecting", err)
+})
 
 app.listen(PORT, () => {
     console.log("server is running on", PORT)
