5fb936a355ddb9096fb7b5a3bbebedafc47261de
andrei.odobesteanu <alexandru.odobesteanu@yahoo.com>
2021-12-10 20:47:54 +0200

    START FRONTEND REACT + DEFINITIVARE SCHEMA CONTAINERE

--- server/routes/container.js
+++ server/routes/container.js
@@ -3,6 +3,43 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Container = mongoose.model("Container")
 const requireLogin = require('../middleware/requireLogin')
+const { SECRET } = require('../keys')
+const { IV } = require('../keys')
+
+
+var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    password = SECRET,
+    iv = IV;
+
+function encrypt(text) {
+    var cipher = crypto.createCipheriv(algorithm, password, iv)
+    var crypted = cipher.update(text, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+// function decrypt(text) {
+//     var decipher = crypto.createDecipheriv(algorithm, password, iv)
+//     var dec = decipher.update(text, 'hex', 'utf8')
+//     dec += decipher.final('utf8');
+//     return dec;
+// }
+
+router.get('/allcontainers', (req, res) => {
+    Container.find()
+        .populate("ownedBy", "_id name")
+        .then(containers => {
+            res.json({ containers })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+
+})
+
+
 
 router.post('/addcontainer', requireLogin, (req, res) => {
     const { host, username, password } = req.body
@@ -12,7 +49,7 @@ router.post('/addcontainer', requireLogin, (req, res) => {
     const container = new Container({
         host: host,
         username: username,
-        password: password,
+        password: encrypt(password),
         ownedBy: req.user
     })
     container.save().then(result => {
@@ -21,7 +58,18 @@ router.post('/addcontainer', requireLogin, (req, res) => {
         .catch(err => {
             console.log(err)
         })
+})
 
+
+router.get('/mycontainers', requireLogin, (req, res) => {
+    Container.find({ ownedBy: req.user._id })
+        .populate("ownedBy", "host user")
+        .then(mycontainer => {
+            res.json({ mycontainer })
+        })
+        .catch(err => {
+            console.log(err)
+        })
 
 })
 
